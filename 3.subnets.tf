886cc5bfb8ff255dcc0ed8a7798a574a3793cbfb
Chejarla Viswanadh <HR412@sgpltech.com>
2025-06-20 15:08:07 +0530

    updated vpc_id in the code

--- 3.subnets.tf
+++ 3.subnets.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "public-subnets" {
   count             = length(local.new_public_subnet_cidrs)
-  vpc_id            = aws_vpc.id
+  vpc_id            = aws_vpc.Harness-VPC.id
   cidr_block        = element(local.new_public_subnet_cidrs, count.index)
   availability_zone = element(var.azs, count.index)
 
